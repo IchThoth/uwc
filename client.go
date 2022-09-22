0f2091b00921fc361b3329df72d6d6fac56287d7
ichthoth <shalomtata334@gmail.com>
2022-09-22 05:23:56 +0100

    loaded env fileaa and created client struct

---
+++ client.go
@@ -0,0 +1,33 @@
+package unsplashwebclient
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	PhotoApi = "https://api.unsplash.com/photos"
+)
+
+type Client struct {
+	Token  string
+	apiUrl string
+	hc     http.Client
+}
+type SearchResult struct {
+	page int32 `json:"page"`
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	Token := os.Getenv("ACCESS_KEY")
+	SecretToken := os.Getenv("SECRET_KEY")
+
+}

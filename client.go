dded0adc1bd4c6db9e66e3adf4224d72f95c567c
ichthoth <shalomtata334@gmail.com>
2022-09-27 06:36:50 +0100

    c to client

--- client.go
+++ client.go
@@ -13,21 +13,39 @@ const (
 )
 
 type Client struct {
-	Token  string
-	apiUrl string
-	hc     http.Client
-}
-type SearchResult struct {
-	page int32 `json:"page"`
+	Token          string
+	Secret         string
+	hc             http.Client
+	RemainingTimes int32
 }
 
+func NewClient(token string, secret string) *Client {
+	c := http.Client{}
+	return &Client{
+		Token:  token,
+		Secret: secret,
+		hc:     c,
+	}
+}
+
+//Requests that return multiple items (a list of photos, for example) will
+//be paginated into pages of 10 items by default, up to a maximum of 30.
+//The optional page and per_page query parameters can be supplied to define
+//which page and the number of items per page to be returned, respectively.
+//If page is not supplied, the first page will be returned.
+//URL’s for the first, last, next, and previous pages are supplied,
+//if applicable. They are comma-separated and differentiated with a rel attribute.
+
 func main() {
-	err := godotenv.Load(".env")
+	//load environment variables
+	err := godotenv.Load("uwc.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	Token := os.Getenv("ACCESS_KEY")
-	SecretToken := os.Getenv("SECRET_KEY")
+	AccessToken := os.Getenv("ACCESS_KEY")
+	SecretKey := os.Getenv("SECRET_KEY ")
+
+	var client = NewClient(AccessToken, SecretKey)
 
 }
